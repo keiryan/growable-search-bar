4bcb561e3205699ab7c16359123e42eecec281b8
Keiryan Wilson <keiryanwilson@gmail.com>
2022-11-24 17:48:06 -0500

    removed extra inputs

--- src/App.js
+++ src/App.js
@@ -6,41 +6,6 @@ function App() {
   return (
     <Container>
       <Search />
-      <Search
-        config={{
-          placeholder: "Search",
-          color: "#ffffff",
-          backgroundColor: "#19535F",
-          border: "2px solid orange",
-          fontWeight: 'bold',
-          searchButtonColorHover: '#ffffff',
-          searchButtonContainerColorHover: '#FFA500',
-        }}
-      />
-      <Search
-        config={{
-          placeholder: "Search",
-          color: "#000",
-          backgroundColor: "#ffffff",
-          border: "2px solid #fff",
-          placeholderWeight: "bold",
-          placeholderColor: '#000000',
-          fontWeight: 'bold',
-          searchButtonColor: 'blue',
-          searchButtonColorHover: '#000000',
-          searchButtonContainerColor: '#ffffff',
-          searchButtonContainerColorHover: 'red',
-        }}
-      />
-
-      <Search
-        config={{
-          placeholder: "Search",
-          color: "#ffffff",
-          backgroundColor: "#999AC6",
-          border: "2px solid #fff",
-        }}
-      />
     </Container>
   );
 }
